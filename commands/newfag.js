6a9cda2a9057de409ddfa04b71c1adfe7c511ed9
Rasmus Karlsson <pajlada@bithack.se>
2015-05-03 19:08:45 +0000

    Added a newfag command.

---
+++ commands/newfag.js
@@ -0,0 +1,10 @@
+exports.names = ['.newfag'];
+exports.hidden = false;
+exports.enabled = true;
+exports.matchStart = true;
+exports.cd_all = 15;
+exports.cd_user = 30;
+exports.cd_manager = 5;
+exports.handler = function (data) {
+    bot.sendChat('/me How to DJ: http://support.plug.dj/hc/en-us/articles/203263577-I-want-to-DJ-How-do-I-do-it-');
+};
