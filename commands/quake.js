5343b9275017bf3a12001fa7fe3931fab3d4ea54
Rasmus Karlsson <pajlada@bithack.se>
2015-05-04 13:55:09 +0000

    Re-added the .quake command

--- commands/quake.js
+++ commands/quake.js
@@ -2,10 +2,13 @@ exports.names = ['.quake'];
 exports.hidden = true;
 exports.enabled = true;
 exports.matchStart = false;
+exports.cd_all = 15;
+exports.cd_user = 30;
+exports.cd_manager = 10;
 exports.handler = function (data) {
     request('http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_day.geojson', function (error, response, body) {
         var quakes = JSON.parse(body).features.slice(0, 3);
-        bot.sendChat('Recent earthquakes: ' + _.map(quakes, function (quake) {
+        bot.sendChat('/me Recent earthquakes: ' + _.map(quakes, function (quake) {
             var timeElapsed = new Date() - new Date(quake.properties.time);
             return quake.properties.title + ' (' + Math.floor(timeElapsed / 3600000) + 'h ' + Math.floor((timeElapsed % 3600000) / 60000) + 'm ago)';
         }).join(' · '));
