59de73947505c7322b5161ae71e30984fc253fa7
Rasmus Karlsson <pajlada@bithack.se>
2015-05-10 16:07:18 +0000

    Added .ban and .unban commands.
    Fixes #10.

---
+++ commands/ban.js
@@ -0,0 +1,81 @@
+exports.names = ['.ban'];
+exports.hidden = false;
+exports.enabled = true;
+exports.matchStart = true;
+exports.cd_all = 5;
+exports.cd_user = 10;
+exports.cd_manager = 0;
+exports.handler = function (data) {
+    if (data.from.role > 2 || data.from.username == 'PAJLADA' || data.from.username == 'RosenMVP') {
+        var input = data.message.split(' ');
+        var command = _.first(input);
+        var params = _.rest(input);
+        var username = '';
+
+        if (params.length >= 2) {
+            username = _.initial(params).join(' ').trim();
+            duration = _.last(params).toUpperCase();
+        } else if (params.length == 1) {
+            username = params.join(' ').trim();
+            var duration = 'HOUR';
+        } else {
+            bot.sendChat('/me Usage: .ban username [PERMA|DAY|HOUR]');
+            return;
+        }
+
+        var usernameFormatted = S(username).chompLeft('@').s;
+
+        users = bot.getUsers();
+        var user = _.findWhere(users, {username: usernameFormatted});
+
+        if (user !== undefined) {
+            // Don't let bouncers get too feisty (API should prohibit this, but just making sure!
+            if (data.from.role == 2) {
+                duration = 'HOUR';
+            }
+
+            var duration_string;
+            var real_dur = '';
+
+            switch (duration) {
+                case 'DAY':
+                case 'D':
+                    duration_string = 'for one day';
+                    real_dur = 'd';
+                    break;
+
+                case 'HOUR':
+                case 'H':
+                    duration_string = 'for one hour';
+                    real_dur = 'h';
+                    break;
+
+                case 'PERMA':
+                case 'P':
+                    duration_string = 'permanently';
+                    real_dur = 'f';
+                    break;
+
+                default:
+                    bot.sendChat('/me Usage: .ban username [PERMA|DAY|HOUR]');
+                    return;
+            }
+
+            var r = bot.moderateBanUser(user.id, null, real_dur, function() {
+                bot.sendChat('/me [@' + data.from.username + '] Banned ' + usernameFormatted + ' ' + duration_string + '.');
+                logger.info('[BAN] ' + usernameFormatted + ' was banned by ' + data.from.username + ' ' + duration_string);
+                var userData = {
+                    type: 'ban',
+                    details: 'Banned by ' + data.from.username + ' ' + duration_string,
+                    user_id: user.id,
+                    mod_user_id: data.from.id
+                };
+                Karma.create(userData);
+            });
+
+            if (r === false) {
+                bot.sendChat('/me [@' + data.from.userame + '] Couldn\'t ban ' + usernameFormatted + ' for some reason.');
+            }
+        }
+    }
+};
---
+++ commands/unban.js
@@ -0,0 +1,37 @@
+exports.names = ['.unban'];
+exports.hidden = false;
+exports.enabled = true;
+exports.matchStart = true;
+exports.cd_all = 5;
+exports.cd_user = 10;
+exports.cd_manager = 0;
+exports.handler = function (data) {
+    if (data.from.role > 2 || data.from.username == 'PAJLADA' || data.from.username == 'RosenMVP') {
+        var params = _.rest(data.message.split(' '), 1);
+        if (params.length < 1) {
+            bot.sendChat('/me Usage: .unban username');
+            return;
+        }
+
+        username = params.join(' ').trim()
+        usernameFormatted = S(username).chompLeft('@').s;
+
+        User.find({where: {username: usernameFormatted}}).on('success', function (row) {
+            if (row === null) {
+                bot.sendChat('/me No user named ' + usernameFormatted + ' was not found.');
+            } else {
+                bot.moderateUnbanUser(row.id, function() {
+                    bot.sendChat('/me [@'+data.from.username+'] Unbanned ' + row.username + '.');
+                    logger.info('[UNBAN] ' + row.username + ' was unbanned by ' + data.from.username);
+                    var userData = {
+                        type: 'unban',
+                        details: 'Unbanned by ' + data.from.username,
+                        user_id: row.id,
+                        mod_user_id: data.from.id
+                    };
+                    Karma.create(userData);
+                });
+            }
+        });
+    }
+};
