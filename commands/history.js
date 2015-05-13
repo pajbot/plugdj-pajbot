c10913d8d87794f4359804a6b4cd47a4eb0648f6
Rasmus Karlsson <pajlada@bithack.se>
2015-05-13 18:43:11 +0000

    .history is now useable by bouncer+ instead of manager+

--- commands/history.js
+++ commands/history.js
@@ -6,7 +6,7 @@ exports.cd_all = 15;
 exports.cd_user = 30;
 exports.cd_manager = 5;
 exports.handler = function (data) {
-    if (data.from.role > 2 || data.from.username == 'PAJLADA') {
+    if (data.from.role > 1) {
         var input = data.message.split(' ');
         var command = _.first(input);
         var params = _.rest(input);
@@ -23,13 +23,13 @@ exports.handler = function (data) {
                 if (rows != null && rows.length == 1) {
                     var row = rows[0];
                     if (row['format'] == 1) {
-                        bot.sendChat('/me ' + row['author'] + ' - ' + row['title'] + ' (https://youtube.com/watch?v=' + row['cid'] + ') was played ' + (offset+1) + ' songs ago');
+                        modMessage(data, row['author'] + ' - ' + row['title'] + ' (https://youtu.be/' + row['cid'] + ') played ' + (offset+1) + ' songs ago.');
                     } else {
                         var client_id = '73401fe4eb06e6ad2b23368567ed1bae';
                         request('https://api.soundcloud.com/tracks/'+row['cid']+'.json?client_id='+client_id, function (error, response, body) {
                             var json_data = JSON.parse(body);
                             var song_url = json_data.permalink_url;
-                            bot.sendChat('/me ' + row['author'] + ' - ' + row['title'] + ' (' + song_url + ') was played ' + (offset+1) + ' songs ago');
+                            modMessage(data, row['author'] + ' - ' + row['title'] + ' (' + song_url + ') played ' + (offset+1) + ' songs ago.');
                         });
                     }
 
