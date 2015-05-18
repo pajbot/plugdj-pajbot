a226512f6f00c56452b03f757dddbcf596f2e66c
Rasmus Karlsson <pajlada@bithack.se>
2015-05-18 17:41:09 +0000

    .commands now prints a link to a list of commands on github instead.

--- commands/commands.js
+++ commands/commands.js
@@ -6,11 +6,5 @@ exports.cd_all = 15;
 exports.cd_user = 30;
 exports.cd_manager = 10;
 exports.handler = function (data) {
-    if (data.from.role > 2 || data.from.username == 'PAJLADA') {
-        bot.sendChat('Commands: ' + _.compact(_.map(commands, function (command) {
-            if (command.enabled && !command.hidden && _.first(command.names) != '.commands') {
-                return _.first(command.names);
-            }
-        })).join(', '));
-    }
+    bot.sendChat('/me Commands: http://git.io/vTObv');
 };
