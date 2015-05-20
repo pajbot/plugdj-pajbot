fa356702fbd802340d7b9a371672736660c358f2
Rasmus Karlsson <pajlada@bithack.se>
2015-05-20 20:47:28 +0000

    .songinfo should be a lot more accurate now

--- commands/songinfo.js
+++ commands/songinfo.js
@@ -14,43 +14,32 @@ exports.handler = function (data) {
             return;
         }
 
-        console.log('hello');
-        if (media.format == 1) {
-            sequelize.query('SELECT `songs`.`created_at` FROM `songs` WHERE `format`=? AND `cid`=? ORDER BY `songs`.`created_at` DESC',
-                    { replacements: [media.format, media.cid], type: Sequelize.QueryTypes.SELECT}
-                    ).then(function(rows) {
-                        console.log(rows);
+        var song_ids = [];
+        Song.findAll({
+            where: {
+                format: media.format,
+                cid: media.cid,
+            },
+            order: 'updated_at DESC'
+        }).on('success', function (rows) {
+            if (rows && rows.length > 0) {
+                _.each(rows, function (row) {
+                    song_ids.push(row.id);
+                });
+
+                Play.findAll({
+                    where: { song_id: song_ids },
+                    order: 'updated_at DESC'
+                }).on('success', function (rows) {
+                    if (rows && rows.length > 0) {
                         var num_plays = rows.length;
-                        if (num_plays > 1) {
-                            bot.sendChat('/me [@'+data.from.username+'] This song has been played '+(num_plays-1)+' times in my lifetime, last time being ' + moment.utc(rows[1]['created_at']).calendar() + ' (' + moment.utc(rows[1]['created_at']).fromNow() + ')');
-                        } else {
-                            bot.sendChat('/me [@'+data.from.username+'] This song has not been played here in my lifetime.');
-                        }
+
+                        modMessage(data, 'This song has been played '+(num_plays-1)+' times in my lifetime, last time being ' + moment.utc(rows[1]['updated_at']).calendar() + ' (' + moment.utc(rows[1]['updated_at']).fromNow() + ')');
                     }
-                    );
-        } else {
-            var num_plays = 0;
-            sequelize.query('SELECT `songs`.`created_at` FROM `songs` WHERE `format`=? AND `cid`=? ORDER BY `songs`.`created_at` DESC',
-                    { replacements: [media.format, media.cid], type: Sequelize.QueryTypes.SELECT}
-                    ).then(function(rows) {
-                        num_plays += rows.length;
-                    }
-                    )
-                    .then(function() {
-                        sequelize.query('SELECT `created_at` FROM `plays` WHERE `song_id`=? ORDER BY `created_at` DESC',
-                                { replacements: [media.format, media.cid], type: Sequelize.QueryTypes.SELECT}
-                                ).then(function(rows) {
-                                    num_plays += rows.length;
-                                }
-                                );
-                    })
-                    .then(function() {
-                        if (num_plays > 1) {
-                            bot.sendChat('/me [@'+data.from.username+'] This song has been played '+(num_plays-1)+' times in my lifetime.');
-                        } else {
-                            bot.sendChat('/me [@'+data.from.username+'] This song has not been played here in my lifetime.');
-                        }
-                    });
-        }
+                });
+            } else {
+                modMessage(data, 'This song has not been played here in my lifetime.');
+            }
+        });
     }
 };
