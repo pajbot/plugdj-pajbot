617c5eed2e2e1908857f03d26dcada7036735b77
Rasmus Karlsson <pajlada@bithack.se>
2015-05-22 12:22:34 +0000

    Added a .pos command, to check a users position.

---
+++ commands/pos.js
@@ -0,0 +1,48 @@
+exports.names = ['.pos', '.position', '!pos', '!position'];
+exports.hidden = false;
+exports.enabled = true;
+exports.matchStart = true;
+exports.cd_all = 2;
+exports.cd_user = 30;
+exports.cd_manager = 2;
+exports.handler = function (data) {
+    if (data.from.role > 1) {
+        var params = _.rest(data.message.split(' '), 1);
+        var user;
+        if (params.length < 1) {
+            user = data.from;
+        } else {
+            var username_uf = params.join(' ').trim();
+            var username = username_uf.replace('@', '');
+            console.log(username);
+            var _user = _.findWhere(bot.getUsers(), {username: username});
+            if (_user) {
+                user = _user;
+            } else {
+                modMessage(data, username + ' is not here :dansgame:');
+                return;
+            }
+        }
+
+        var pos = bot.getWaitListPosition(user.id);
+        if (pos < 0) {
+            if (user.id == data.from.id) {
+                modMessage(data, 'You\'re not in the waitlist.');
+            } else {
+                modMessage(data, user.username + 'is not in the waitlist.');
+            }
+        } else if (pos == 0) {
+            if (user.id == data.from.id) {
+                modMessage(data, 'You\'re currently playing your song!');
+            } else {
+                modMessage(data, user.username + 'is currently playing his song!');
+            }
+        } else {
+            if (user.id == data.from.id) {
+                modMessage(data, 'You\'re at position ' + pos + ' in the waitlist.');
+            } else {
+                modMessage(data, user.username + ' is at position ' + pos + ' in the waitlist.');
+            }
+        }
+    }
+};
