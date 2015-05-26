337b1ab4593d0e1deae039f7254a0f399d9001e3
Rasmus Karlsson <pajlada@bithack.se>
2015-05-26 14:45:06 +0000

    Added a .kolento command

---
+++ commands/kolento.js
@@ -0,0 +1,10 @@
+exports.names = ['kolento'];
+exports.hidden = false;
+exports.enabled = true;
+exports.matchStart = true;
+exports.cd_all = 15;
+exports.cd_user = 30;
+exports.cd_manager = 10;
+exports.handler = function (data) {
+    chatMessage('/me :kolentohappy: http://i.imgur.com/nj1dvtu.gif');
+};
