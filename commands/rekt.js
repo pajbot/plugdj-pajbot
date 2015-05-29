d74af3bc1f1ece8da33b602656e2f9252dc610ec
Rasmus Karlsson <pajlada@bithack.se>
2015-05-29 02:29:38 +0000

    new command: .rekt

---
+++ commands/rekt.js
@@ -0,0 +1,10 @@
+exports.names = ['rekt'];
+exports.hidden = false;
+exports.enabled = true;
+exports.matchStart = true;
+exports.cd_all = 15;
+exports.cd_user = 30;
+exports.cd_manager = 10;
+exports.handler = function (data) {
+    chatMessage('/me ☐ Not REKT ☑ REKT');
+};
