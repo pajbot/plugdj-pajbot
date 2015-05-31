94683f18a945ec4918587394981c8ee8cf505b7a
Rasmus Karlsson <pajlada@bithack.se>
2015-05-31 18:22:18 +0000

    .quake no longer crashes the bot if the API fails

--- commands/quake.js
+++ commands/quake.js
@@ -7,7 +7,10 @@ exports.cd_user = 30;
 exports.cd_manager = 10;
 exports.handler = function (data) {
     request('http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_day.geojson', function (error, response, body) {
-            logger.info(error);
+        if (error) {
+            logger.error('Error with the quake api: ' + error);
+            return;
+        }
         var quakes = JSON.parse(body).features.slice(0, 3);
         chatMessage('/me Recent earthquakes: ' + _.map(quakes, function (quake) {
             var timeElapsed = new Date() - new Date(quake.properties.time);
