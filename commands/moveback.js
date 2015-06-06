f75b3d464bcac0fe9a14d9eed244bd0c18bc96b4
Rasmus Karlsson <pajlada@bithack.se>
2015-06-06 14:00:21 +0000

    .moveback now uses the new cooldown feature

--- commands/moveback.js
+++ commands/moveback.js
@@ -8,8 +8,11 @@ exports.cd_manager = 0;
 exports.min_role = PERMISSIONS.NONE;
 exports.handler = function (data) {
     var position = bot.getWaitListPosition(data.from.id);
-    if (position !== -1 && position !== 0) {
+    if (position !== -1 && position !== 0 && position !== 50) {
         move_user(data.from.id, 50);
         chatMessage('/me Moving ' + data.from.username + ' back to 50.');
+    } else {
+        modMessage(data, 'You need to be in the waitlist to use .moveback.');
+        return {cd_all: 5, cd_user: 20};
     }
 };
