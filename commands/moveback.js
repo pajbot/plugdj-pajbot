0294dc7bb0f05df5593d6b9d5c0d5ad7a2c8d910
Rasmus Karlsson <pajlada@bithack.se>
2015-06-06 13:05:19 +0000

    Slightly modified the .moveback command
    We need to check if the user is currently playing his song,
       otherwise he could use !moveback when he is playing to be confirmed
       a spot in the queue after he's done playing his song.
    
    Also fixed the indentation and file format of the file.

--- commands/moveback.js
+++ commands/moveback.js
@@ -7,9 +7,9 @@ exports.cd_user = 3600;
 exports.cd_manager = 0;
 exports.min_role = PERMISSIONS.NONE;
 exports.handler = function (data) {
-				var position = bot.getWaitListPosition(data.from.id);
-					if(position !== -1) {
-						 move_user(data.from.id, 50);
-            	 		 chatMessage('/me Moving ' + data.from.username + ' back to 50.');
-            	 		 }
-            	 	};
+    var position = bot.getWaitListPosition(data.from.id);
+    if (position !== -1 && position !== 0) {
+        move_user(data.from.id, 50);
+        chatMessage('/me Moving ' + data.from.username + ' back to 50.');
+    }
+};
