1a6bedd5b5b51dd7a81e1945552e825b89a73c76
Rasmus Karlsson <pajlada@bithack.se>
2015-06-08 04:20:50 +0000

    .swap now uses the new parameter parsing function (experimental)

--- commands/swap.js
+++ commands/swap.js
@@ -1,4 +1,4 @@
-exports.names = ['swap'];
+exports.names = ['swap', 'sw'];
 exports.hidden = true;
 exports.enabled = true;
 exports.matchStart = true;
@@ -9,48 +9,37 @@ exports.min_role = PERMISSIONS.BOUNCER_PLUS;
 exports.handler = function (data) {
     var input = data.message.split(' ');
     var params = _.rest(input).join(' ');
-    if (input.length >= 3) {
-        logger.info(params);
-        var first_pos = params.indexOf('@');
-        var last_pos = params.lastIndexOf('@');
+    parse_command_params(data.message, CPARAM.USERNAME, CPARAM.USERNAME).then(function(pdata) {
+        var user_1 = pdata.params[0].user;
+        var user_2 = pdata.params[1].user;
 
-        if (first_pos !== -1 && first_pos !== last_pos && last_pos !== params.length) {
-            var username_1 = params.substr(first_pos + 1, last_pos - first_pos - 2);
-            var username_2 = params.substr(last_pos + 1);
+        if (user_1 && user_2) {
+            var position_1 = bot.getWaitListPosition(user_1.id);
+            var position_2 = bot.getWaitListPosition(user_2.id);
 
-            var users = bot.getUsers();
-            var user_1 = _.findWhere(users, {username: username_1});
-            var user_2 = _.findWhere(users, {username: username_2});
-            if (user_1 !== undefined && user_2 !== undefined) {
-                var position_1 = bot.getWaitListPosition(user_1.id);
-                var position_2 = bot.getWaitListPosition(user_2.id);
+            logger.info(position_1);
+            logger.info(position_2);
 
-                logger.info(position_1);
-                logger.info(position_2);
+            if (position_1 !== -1 || position_2 !== -1) {
+                chatMessage('/me [@' + data.from.username + '] Swapping ' + user_1.username + ' with ' + user_2.username + '.');
 
-                if (position_1 !== -1 || position_2 !== -1) {
-                    chatMessage('/me [@' + data.from.username + '] Swapping ' + username_1 + ' with ' + username_2 + '.');
+                logger.info(user_1.id + ' to position ' + position_2);
+                logger.info(user_2.id + ' to position ' + position_1);
 
-                    logger.info(user_1.id + ' to position ' + position_2);
-                    logger.info(user_2.id + ' to position ' + position_1);
-
-                    if (position_2 === -1) {
-                        move_user(user_2.id, position_1);
-                        setTimeout(function() {
-                            move_user(user_1.id, position_2);
-                        }, 500);
-                    } else {
+                if (position_2 === -1) {
+                    move_user(user_2.id, position_1);
+                    setTimeout(function() {
                         move_user(user_1.id, position_2);
-                        setTimeout(function() {
-                            move_user(user_2.id, position_1);
-                        }, 500);
-                    }
+                    }, 500);
+                } else {
+                    move_user(user_1.id, position_2);
+                    setTimeout(function() {
+                        move_user(user_2.id, position_1);
+                    }, 500);
                 }
-            } else {
-                modMessage(data, 'Invalid users specified.');
             }
         } else {
-            modMessage(data, 'Usage: .swap @USER1 @USER2');
+            modMessage(data, 'Invalid users specified.');
         }
-    }
+    });
 }
