00b9e6aa7b90b2d008cee4b004bcff838e2ebf67
Rasmus Karlsson <pajlada@bithack.se>
2015-06-09 14:01:45 +0000

    Formatting fixes for swaprequest

--- commands/swaprequest.js
+++ commands/swaprequest.js
@@ -33,7 +33,6 @@ exports.handler = function (data) {
 
     switch (command) {
         case 'swaprequest':
-
             get_user_by_param(params, function(err, user, db_user) {
                 if (user) {
                     var time_diff_user = cur_time;
@@ -48,18 +47,18 @@ exports.handler = function (data) {
 
                     var current_position = bot.getWaitListPosition(data.from.id);
                     var partner_position = bot.getWaitListPosition(user.id);
-                        
+
                     if (current_position === -1 && partner_position === -1) {
                         logger.info('At least one of you must be in the waitlist to swap!.');
                         modMessage(data, 'You and ' + user.username + ' must be in the waitlist to swap.');
                         /* Someone needs to have a spot in list to swap. */
                         return {cd: 2, cd_user: 10};
                     } else { 
-                    	if (current_position === 0 || partner_position === 0) {
-                        modMessage(data, 'You or ' + user.username + ' can\'t be playing your songs when you want to swap.');
-                        /* You must be in the waitlist to swap someone. */
-                        return {cd: 2, cd_user: 10};
-                   		} 
+                        if (current_position === 0 || partner_position === 0) {
+                            modMessage(data, 'You or ' + user.username + ' can\'t be playing your songs when you want to swap.');
+                            /* You must be in the waitlist to swap someone. */
+                            return {cd: 2, cd_user: 10};
+                        } 
                     }
 
                     if (user.id === data.from.id) {
@@ -120,32 +119,32 @@ exports.handler = function (data) {
                     user_2 = req.target;
 
                     if (user_1 && user_2) {
-            		var position_1 = bot.getWaitListPosition(user_1.id);
-            		var position_2 = bot.getWaitListPosition(user_2.id);
+                    var position_1 = bot.getWaitListPosition(user_1.id);
+                    var position_2 = bot.getWaitListPosition(user_2.id);
 
-            		logger.info(position_1);
-            		logger.info(position_2);
+                    logger.info(position_1);
+                    logger.info(position_2);
 
-		            if (position_1 !== -1 || position_2 !== -1) {
-		                chatMessage('/me [@' + data.from.username + '] Swapping ' + user_1.username + ' with ' + user_2.username + '.');
-		
-		                logger.info(user_1.id + ' to position ' + position_2);
-		                logger.info(user_2.id + ' to position ' + position_1);
-		
-		                if (position_2 === -1) {
-		                    move_user(user_2.id, position_1);
-		                    setTimeout(function() {
-		                        move_user(user_1.id, position_2);
-		                    }, 500);
-		                } else {
-		                    move_user(user_1.id, position_2);
-		                    setTimeout(function() {
-		                        move_user(user_2.id, position_1);
-		                    }, 500);
-		                }
-		            }
-		       } else {
-		            modMessage(data, 'Invalid users specified.');
+                    if (position_1 !== -1 || position_2 !== -1) {
+                        chatMessage('/me [@' + data.from.username + '] Swapping ' + user_1.username + ' with ' + user_2.username + '.');
+        
+                        logger.info(user_1.id + ' to position ' + position_2);
+                        logger.info(user_2.id + ' to position ' + position_1);
+        
+                        if (position_2 === -1) {
+                            move_user(user_2.id, position_1);
+                            setTimeout(function() {
+                                move_user(user_1.id, position_2);
+                            }, 500);
+                        } else {
+                            move_user(user_1.id, position_2);
+                            setTimeout(function() {
+                                move_user(user_2.id, position_1);
+                            }, 500);
+                        }
+                    }
+               } else {
+                    modMessage(data, 'Invalid users specified.');
         }
     
 
