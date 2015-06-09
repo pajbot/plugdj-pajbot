15dc3e4b0bb806359b6a32b552064a8153054949
Rasmus Karlsson <pajlada@bithack.se>
2015-06-09 14:09:27 +0000

    .swaprequest now uses the swap_users function

--- commands/swaprequest.js
+++ commands/swaprequest.js
@@ -111,46 +111,15 @@ exports.handler = function (data) {
 
                 modMessage(data, 'You accepted the swap request from @' + req.requestor.username + '. Commencing swap!');
                 last_swap = cur_time;
+
                 setTimeout(function() {
                     if (swap_request === false) {
                         chatMessage('/me You can now swap again.');
                     }
                 }, swap_cd * 1000);
+
                 setTimeout(function() {
-
-                    user_1 = req.requestor;
-                    user_2 = req.target;
-
-                    if (user_1 && user_2) {
-                        var position_1 = bot.getWaitListPosition(user_1.id);
-                        var position_2 = bot.getWaitListPosition(user_2.id);
-
-                        logger.info(position_1);
-                        logger.info(position_2);
-
-                        if (position_1 !== -1 || position_2 !== -1) {
-                            chatMessage('/me [@' + data.from.username + '] Swapping ' + user_1.username + ' with ' + user_2.username + '.');
-
-                            logger.info(user_1.id + ' to position ' + position_2);
-                            logger.info(user_2.id + ' to position ' + position_1);
-
-                            if (position_2 === -1) {
-                                move_user(user_2.id, position_1);
-                                setTimeout(function() {
-                                    move_user(user_1.id, position_2);
-                                }, 500);
-                            } else {
-                                move_user(user_1.id, position_2);
-                                setTimeout(function() {
-                                    move_user(user_2.id, position_1);
-                                }, 500);
-                            }
-                        }
-                    } else {
-                        modMessage(data, 'Invalid users specified.');
-                    }
-
-
+                    swap_users(req.requestor, req.target);
                     swap_request = false;
                 }, 1000);
             } else {
