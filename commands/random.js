5529864f494c6f0c28ac7e5f902de77ec98784dc
Leti460 <Leti460@users.noreply.github.com>
2015-06-14 20:03:50 +0200

    '.random x' will give a random value between 1 and x. If x is not specified, it will toss a coin instead.

---
+++ commands/random.js
@@ -0,0 +1,26 @@
+exports.names = ['random'];
+exports.hidden = false;
+exports.enabled = true;
+exports.matchStart = true;
+exports.cd_all = 15;
+exports.cd_user = 60;
+exports.cd_manager = 0;
+exports.min_role = PERMISSIONS.NONE;
+exports.handler = function (data) {
+    var input = data.message.split(' ');
+    var range = _.last(input);
+	var result = '';
+   
+			
+    if (input.length >= 2) {
+		result = _.random(1, range);
+	} else { 
+		var x = _.random(1, 2);
+		if (x = 1) {
+			 result = 'Heads';
+		} else {
+			result = 'Tails';
+		}
+	};
+    chatMessage('/me [@' + data.from.username + '] Your random result is: ' + result + '.');
+};
