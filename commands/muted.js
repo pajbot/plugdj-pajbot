8ce0251deda9ebec7eaa99575259dfde9e4c2ca5
Rasmus Karlsson <pajlada@bithack.se>
2015-06-15 16:28:58 +0000

    Improved .muted
    It now takes another optional argument, length of mute.
    It also uses the parse_command_params function.

--- commands/muted.js
+++ commands/muted.js
@@ -7,5 +7,20 @@ exports.cd_user = 2;
 exports.cd_manager = 0;
 exports.min_role = PERMISSIONS.BOUNCER;
 exports.handler = function (data) {
-    assist(data.message, 'Do not ask for skips. You have been muted for 15 minutes.');
+    parse_command_params(data.message, CPARAM.USERNAME, CPARAM.INT).then(function(cdata) {
+        logger.info(cdata);
+        var mute_duration = 15;
+        var valid_durations = [15, 30, 45];
+        if (cdata.params[1] !== undefined && cdata.params[1] && valid_durations.indexOf(cdata.params[1]) !== -1) {
+            mute_duration = cdata.params[1];
+        }
+        
+        var at_user = '';
+        
+        if (cdata.params[0] !== undefined && cdata.params[0].user) {
+            at_user = '@' + cdata.params[0].user.username + ' ';
+        }
+        
+        chatMessage('/me ' + at_user + 'Do not ask for skips. You have been muted for ' + mute_duration + ' minutes.');
+    });
 };
