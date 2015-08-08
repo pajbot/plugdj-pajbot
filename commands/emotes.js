6e3dedac1d9d9c3cdd5b233144e4e37c96e1eae8
Leti460 <Leti460@users.noreply.github.com>
2015-08-08 19:55:17 +0200

    New .emotes links to the RCS emotes list.

---
+++ commands/emotes.js
@@ -0,0 +1,10 @@
+exports.names = ['emotes'];
+exports.hidden = false;
+exports.enabled = true;
+exports.matchStart = true;
+exports.cd_all = 10;
+exports.cd_user = 20;
+exports.cd_manager = 5;
+exports.handler = function (data) {
+    assist(data.message, 'All RCS emotes can be found here: https://rcs.radiant.dj/emotes');
+};
