d4cd926897e02c81fc67e8eb9d3b4832a5e11fc0
Rasmus Karlsson <pajlada@bithack.se>
2015-09-15 13:26:42 +0000

    reload no longer outputs a message

--- commands/reload.js
+++ commands/reload.js
@@ -7,7 +7,5 @@ exports.cd_user = 30;
 exports.cd_manager = 0;
 exports.min_role = PERMISSIONS.COHOST;
 exports.handler = function (data) {
-    modMessage(data, 'Reloading commands...');
-
     load_responses();
 };
