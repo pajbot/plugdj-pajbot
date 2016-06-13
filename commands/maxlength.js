e98798facf174b4e324a1e93677c584f93f515cc
Leti460 <Leti460@users.noreply.github.com>
2016-06-14 00:49:11 +0200

    maxlength cannot be set lower than 5 minutes anymore.

--- commands/maxlength.js
+++ commands/maxlength.js
@@ -7,9 +7,9 @@ exports.cd_user = 0;
 exports.cd_manager = 0;
 exports.min_role = PERMISSIONS.BOUNCER_PLUS;
 exports.handler = function (data) {
-var input = data.message.split(' ');
-var command = _.first(input);
-var params = _.rest(input);
+	var input = data.message.split(' ');
+	var command = _.first(input);
+	var params = _.rest(input);
 	if (params.length < 1) {
 		modMessage(data, 'Current max song length: ' + sec_to_str(settings['maxlength']) + '.');
 	}
@@ -20,14 +20,23 @@ var params = _.rest(input);
 		
 		if (!isNaN(new_minutes) && !isNaN(new_seconds)) {
 			var new_length = Math.floor(new_minutes * 60) + Math.floor(new_seconds);
-			set_setting('maxlength', new_length, data);
-			modMessage(data, 'Max song length set to ' + sec_to_str(settings['maxlength']) + '.');
-		}
+			if (new_length >= 300) {
+				set_setting('maxlength', new_length, data);
+				modMessage(data, 'Max song length set to ' + sec_to_str(settings['maxlength']) + '.');
+			}
+			else {
+				modMessage(data, 'You can't set the max song length below 5 minutes. :omgscoots:');
+			}
 	} else {
 		var new_length = parseFloat(params);
 		if (!isNaN(new_length)) {
-		set_setting('maxlength', Math.floor(new_length * 60), data);
-		modMessage(data, 'Max song length set to ' + sec_to_str(settings['maxlength']) + '.');
+			if (Math.floor(new_length * 60) >= 300) {
+				set_setting('maxlength', Math.floor(new_length * 60), data);
+				modMessage(data, 'Max song length set to ' + sec_to_str(settings['maxlength']) + '.');
+			}
+			else {
+				modMessage(data, 'You can't set the max song length below 5 minutes. :omgscoots:');
+			}
 		}
 	}
 };
