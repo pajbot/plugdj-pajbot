ac1a3f7c710ff1f32017f4e06c9352a0cf339b36
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2017-12-08 21:25:22 +0000

    Add catfacts commands in a try/catch

--- commands/catfact.js
+++ commands/catfact.js
@@ -7,7 +7,11 @@ exports.cd_user = 30;
 exports.cd_manager = 10;
 exports.min_role = PERMISSIONS.RDJ;
 exports.handler = function (data) {
-    request('http://catfacts-api.appspot.com/api/facts', function (error, response, body) {
-        chatMessage(JSON.parse(body).facts[0] + ' :smartcat:');
-    });
+        request('http://catfacts-api.appspot.com/api/facts', function (error, response, body) {
+            try {
+                chatMessage(JSON.parse(body).facts[0] + ' :smartcat:');
+            } catch (e) {
+                console.log(e);
+            }
+        });
 };
